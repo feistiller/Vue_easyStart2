404d788355c5ac5d7aa20eac9b9a4b89c5fa7c4d
stiller <uneedzf@gmail.com>
2017-10-19 14:26:05 +0800

    更新所有的路由

--- book_service/book_service/routes/admin.js
+++ book_service/book_service/routes/admin.js
@@ -6,19 +6,63 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-// /movie/list
-// /movie/download
-// /admin/movieAdd
-// /admin/movieDel
-// /admin/movieUpdate
-// /admin/movie
-// /movie/detail
-// /movie/comment
-// /movie/showNumber
-// /admin/movieAdd
-// /admin/movieDel
-// /admin/movieUpdate
-// /admin/movie
+router.post('/admin/movieAdd', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
 
+router.post('/admin/movieDel', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/movieUpdate', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/movie', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/checkComment', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/delComment', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/stopUser', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+router.post('/admin/changeUser', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/showUser', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/powerUpdate', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/addArticle', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/addRecommend', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/delRecommend', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/updateRecommend', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/admin/delArticle', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
--- book_service/book_service/routes/index.js
+++ book_service/book_service/routes/index.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 /* GET home page. */
+//主页
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+//mongoose测试
 router.get('/mongooseTest', function (req, res, next) {
     mongoose.connect('mongodb://localhost/pets', { useMongoClient: true });
     mongoose.Promise = global.Promise;
@@ -22,6 +23,26 @@ router.get('/mongooseTest', function (req, res, next) {
         }
     });
     res.send('数据库连接测试');
+});
+//显示主页的推荐大图等
+router.post('/showIndex', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+//显示所有的排行榜，也就是对于电影字段index的样式
+router.post('/showRanking', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+//显示文章列表
+router.post('/showArticle', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+//显示文章的内容
+router.post('/articleDetail', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+//显示用户自己个人信息的内容
+router.post('/showUser', function(req, res, next) {
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;

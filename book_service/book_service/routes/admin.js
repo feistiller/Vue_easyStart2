02289077341de8566002c2ba113047fec034a9f5
stiller <uneedzf@gmail.com>
2017-10-23 19:16:21 +0800

    更新应用

--- book_service/book_service/routes/admin.js
+++ book_service/book_service/routes/admin.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 var user = require('../models/user');
 var movie = require('../models/movie');
-var comment =require('../models/comment');
-var article=require('../models/article');
-var recommend=require('../models/recommend');
+var comment = require('../models/comment');
+var article = require('../models/article');
+var recommend = require('../models/recommend');
 var crypto = require('crypto');
 const init_token = 'TKL02o'
 
 //后台管理需要验证其用户的后台管理权限
 //后台管理admin，添加新的电影
-router.post('/admin/movieAdd', function (req, res, next) {
+router.post('/movieAdd', function (req, res, next) {
 
     if (!req.body.username) {
         res.json({status: 1, message: "用户名为空"})
@@ -36,50 +36,68 @@ router.post('/admin/movieAdd', function (req, res, next) {
     //验证
     var check = checkAdminPower(req.body.username, req.body.token, req.body.id)
     if (check.error == 0) {
-        //    有权限的情况下
-        var saveMovie = new movie({
-            movieName: req.body.movieName,
-            movieImg: req.body.movieImg,
-            movieVideo: req.body.movieVideo,
-            movieDownload: req.bodymovieDownload,
-            movieTime: Date.now(),
-            movieNumSuppose: 0,
-            movieNumDownload: 0,
-            movieMainPage: movieMainPage,
+        //    验证用户的情况下
+        user.findByUsername(req.body.username, function (err, findUser) {
+            if (findUser[0].userAdmin && !findUser[0].userStop) {
+                var saveMovie = new movie({
+                    movieName: req.body.movieName,
+                    movieImg: req.body.movieImg,
+                    movieVideo: req.body.movieVideo,
+                    movieDownload: req.body.movieDownload,
+                    movieTime: Date.now(),
+                    movieNumSuppose: 0,
+                    movieNumDownload: 0,
+                    movieMainPage: movieMainPage,
+                })
+                saveMovie.save(function (err) {
+                    if (err) {
+                        res.json({status: 1, message: err})
+                    } else {
+                        res.json({status: 0, message: "添加成功"})
+                    }
+                })
+            } else {
+                res.json({error: 1, message: "用户没有获得权限或者已经停用"})
+            }
         })
-        saveMovie.save(function (err) {
-            res.json({status: 1, message: err})
-        })
+
     } else {
         res.json({status: 1, message: check.message})
     }
 });
 //删除后台添加的电影条目
-router.post('/admin/movieDel', function (req, res, next) {
-    if (!req.body.movieId) {
-        res.json({status: 1, message: "电影id传递失败"})
+router.post('/movieDel', function (req, res, next) {
+        if (!req.body.movieId) {
+            res.json({status: 1, message: "电影id传递失败"})
+        }
+        if (!req.body.username) {
+            res.json({status: 1, message: "用户名为空"})
+        }
+        if (!req.body.token) {
+            res.json({status: 1, message: "登录出错"})
+        }
+        if (!req.body.id) {
+            res.json({status: 1, message: "用户传递错误"})
+        }
+        //验证
+        var check = checkAdminPower(req.body.username, req.body.token, req.body.id)
+        if (check.error == 0) {
+            user.findByUsername(req.body.username, function (err, findUser) {
+                if (findUser[0].userAdmin && !findUser[0].userStop) {
+                    movie.remove({_id: req.body.movieId}, function (err, delMovie) {
+                        res.json({status: 0, message: '删除成功', data: delMovie})
+                    })
+                } else {
+                    res.json({error: 1, message: "用户没有获得权限或者已经停用"})
+                }
+            })
+        } else {
+            res.json({status: 1, message: check.message})
+        }
     }
-    if (!req.body.username) {
-        res.json({status: 1, message: "用户名为空"})
-    }
-    if (!req.body.token) {
-        res.json({status: 1, message: "登录出错"})
-    }
-    if (!req.body.id) {
-        res.json({status: 1, message: "用户传递错误"})
-    }
-    //验证
-    var check = checkAdminPower(req.body.username, req.body.token, req.body.id)
-    if (check.error == 0) {
-        movie.remove({_id: req.body.movieId}, function (err, delMovie) {
-            res.json({status: 0, message: '删除成功', data: delMovie})
-        })
-    } else {
-        res.json({status: 1, message: check.message})
-    }
-});
+);
 //修改后台添加的条目
-router.post('/admin/movieUpdate', function (req, res, next) {
+router.post('/movieUpdate', function (req, res, next) {
     if (!req.body.movieId) {
         res.json({status: 1, message: "电影id传递失败"})
     }
@@ -94,31 +112,38 @@ router.post('/admin/movieUpdate', function (req, res, next) {
     }
 
     //这里在前台打包一个电影对象全部发送至后台直接存储
-    var saveData=req.body.movieInfo
+    var saveData = req.body.movieInfo
     //验证
     var check = checkAdminPower(req.body.username, req.body.token, req.body.id)
     if (check.error == 0) {
-        movie.update({_id: req.body.movieId},saveData, function (err, delMovie) {
-            res.json({status: 0, message: '删除成功', data: delMovie})
+        user.findByUsername(req.body.username, function (err, findUser) {
+            if (findUser[0].userAdmin && !findUser[0].userStop) {
+                movie.update({_id: req.body.movieId}, saveData, function (err, delMovie) {
+                    res.json({status: 0, message: '删除成功', data: delMovie})
+                })
+            } else {
+                res.json({error: 1, message: "用户没有获得权限或者已经停用"})
+            }
+
         })
     } else {
         res.json({status: 1, message: check.message})
     }
 });
 // 显示后台的所有电影
-router.post('/admin/movie', function (req, res, next) {
+router.get('/movie', function (req, res, next) {
     movie.findAll(function (err, allMovie) {
         res.json({status: 0, message: '获取成功', data: allMovie})
     })
 });
 //显示后台的所有评论
-router.post('/admin/commentsList', function (req, res, next) {
+router.get('/commentsList', function (req, res, next) {
     comment.findAll(function (err, allComment) {
         res.json({status: 0, message: '获取成功', data: allComment})
     })
 });
 //将评论进行审核
-router.post('/admin/checkComment', function (req, res, next) {
+router.post('/checkComment', function (req, res, next) {
     if (!req.body.commentId) {
         res.json({status: 1, message: "评论id传递失败"})
     }
@@ -133,15 +158,21 @@ router.post('/admin/checkComment', function (req, res, next) {
     }
     var check = checkAdminPower(req.body.username, req.body.token, req.body.id)
     if (check.error == 0) {
-        comment.update({_id: req.body.commentId},{check:true}, function (err, updateComment) {
-            res.json({status: 0, message: '审核成功', data: updateComment})
+        user.findByUsername(req.body.username, function (err, findUser) {
+            if (findUser[0].userAdmin && !findUser[0].userStop) {
+                comment.update({_id: req.body.commentId}, {check: true}, function (err, updateComment) {
+                    res.json({status: 0, message: '审核成功', data: updateComment})
+                })
+            } else {
+                res.json({error: 1, message: "用户没有获得权限或者已经停用"})
+            }
         })
     } else {
         res.json({status: 1, message: check.message})
     }
 });
 //对于用户的评论删除
-router.post('/admin/delComment', function (req, res, next) {
+router.post('/delComment', function (req, res, next) {
     if (!req.body.commentId) {
         res.json({status: 1, message: "评论id传递失败"})
     }
@@ -156,15 +187,21 @@ router.post('/admin/delComment', function (req, res, next) {
     }
     var check = checkAdminPower(req.body.username, req.body.token, req.body.id)
     if (check.error == 0) {
-        comment.remove({_id: req.body.commentId}, function (err, delComment) {
-            res.json({status: 0, message: '删除成功', data: delComment})
+        user.findByUsername(req.body.username, function (err, findUser) {
+            if (findUser[0].userAdmin && !findUser[0].userStop) {
+                comment.remove({_id: req.body.commentId}, function (err, delComment) {
+                    res.json({status: 0, message: '删除成功', data: delComment})
+                })
+            } else {
+                res.json({error: 1, message: "用户没有获得权限或者已经停用"})
+            }
         })
     } else {
         res.json({status: 1, message: check.message})
     }
 });
 // 封停用户
-router.post('/admin/stopUser', function (req, res, next) {
+router.post('/stopUser', function (req, res, next) {
     if (!req.body.userId) {
         res.json({status: 1, message: "用户id传递失败"})
     }
@@ -179,9 +216,16 @@ router.post('/admin/stopUser', function (req, res, next) {
     }
     var check = checkAdminPower(req.body.username, req.body.token, req.body.id)
     if (check.error == 0) {
-        user.update({_id: req.body.userId},{userStop:true}, function (err, updateUser) {
-            res.json({status: 0, message: '封停成功', data: updateUser})
+        user.findByUsername(req.body.username, function (err, findUser) {
+            if (findUser[0].userAdmin && !findUser[0].userStop) {
+                user.update({_id: req.body.userId}, {userStop: true}, function (err, updateUser) {
+                    res.json({status: 0, message: '封停成功', data: updateUser})
+                })
+            } else {
+                res.json({error: 1, message: "用户没有获得权限或者已经停用"})
+            }
         })
+
     } else {
         res.json({status: 1, message: check.message})
     }
@@ -205,7 +249,7 @@ router.post('/admin/changeUser', function (req, res, next) {
     }
     var check = checkAdminPower(req.body.username, req.body.token, req.body.id)
     if (check.error == 0) {
-        user.update({_id: req.body.userId},{password:req.body.newPassword}, function (err, updateUser) {
+        user.update({_id: req.body.userId}, {password: req.body.newPassword}, function (err, updateUser) {
             res.json({status: 0, message: '修改成功', data: updateUser})
         })
     } else {
@@ -251,7 +295,7 @@ router.post('/admin/powerUpdate', function (req, res, next) {
     }
     var check = checkAdminPower(req.body.username, req.body.token, req.body.id)
     if (check.error == 0) {
-        user.update({_id: req.body.userId},{userAdmin:true}, function (err, updateUser) {
+        user.update({_id: req.body.userId}, {userAdmin: true}, function (err, updateUser) {
             res.json({status: 0, message: '修改成功', data: updateUser})
         })
     } else {
@@ -278,9 +322,9 @@ router.post('/admin/addArticle', function (req, res, next) {
     if (check.error == 0) {
         //    有权限的情况下
         var saveArticle = new article({
-            articleTitle:req.body.articleTitle,
-            articleContext:req.body.articleContext,
-            articleTime:Date.now()
+            articleTitle: req.body.articleTitle,
+            articleContext: req.body.articleContext,
+            articleTime: Date.now()
         })
         saveArticle.save(function (err) {
             res.json({status: 1, message: err})
@@ -339,9 +383,9 @@ router.post('/admin/addRecommend', function (req, res, next) {
     if (check.error == 0) {
         //    有权限的情况下
         var saveRecommend = new recommend({
-            recommendImg:req.body.recommendImg,
-            recommendSrc:req.body.recommendSrc,
-            recommendTitle:req.body.recommendTitle
+            recommendImg: req.body.recommendImg,
+            recommendSrc: req.body.recommendSrc,
+            recommendTitle: req.body.recommendTitle
         })
         saveRecommend.save(function (err) {
             res.json({status: 1, message: err})
@@ -378,13 +422,14 @@ router.post('/admin/delRecommend', function (req, res, next) {
 //验证用户的token和登录状态
 function checkAdminPower(name, token, id) {
     if (token == getMD5Password(id)) {
-        user.findByUsername(name, function (err, findUser) {
-            if (findUser) {
-                return {error: 0, data: findUser}
-            } else {
-                return {error: 1, message: "用户为获得"}
-            }
-        })
+        return {error: 0, message: "用户登录成功"}
+        // user.findByUsername(name, function (err, findUser) {
+        //     if (findUser) {
+        //         return {error: 0, data: findUser}
+        //     } else {
+        //         return {error: 1, message: "用户为获得"}
+        //     }
+        // })
     } else {
         return {error: 1, message: "用户登录错误"}
     }
